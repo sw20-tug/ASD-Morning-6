500557a1aefe50429519d42bfbac6df14defebed
Martin Laubreiter <laubreiter@student.tugraz.at>
2020-03-18 11:37:49 +0100

    [COOK-000] added unit test support and added functionality test for unit tests (#12)

---
+++ src/test/ControllerTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+    @Test
+    public void functionTest(){
+        System.out.println("this is a functionality test");
+        assert(true);
+    }
+}
