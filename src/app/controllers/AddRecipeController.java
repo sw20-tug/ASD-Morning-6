d1ba194cdd80ad0bcc64fc118636b5b61d9f6054
LookaVooka <lord1601@gmail.com>
2020-03-25 13:14:39 +0100

    COOK-002 added UI fw & script [DI, LW]

--- src/app/controllers/AddRecipeController.java
+++ src/app/controllers/AddRecipeController.java
@@ -1,4 +1,6 @@
 package app.controllers;
+import app.enums.MealType;
+import app.models.RecipeManager;
 import javafx.event.ActionEvent;
 import app.models.Recipe;
 import javafx.collections.ObservableList;
@@ -8,9 +10,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 //import java.awt.event.ActionEvent;
+import javax.swing.text.StyledEditorKit;
 import java.net.URL;
+import java.time.Duration;
 import java.util.ResourceBundle;
 
 
@@ -39,6 +44,9 @@ public class AddRecipeController implements Initializable {
 
     @FXML
     private Button btn_save;
+
+    @FXML
+    private  Button btn_cancel;
 
     public AddRecipeController(int inparam1) {
         this.param1 = inparam1;
@@ -71,9 +79,68 @@ public class AddRecipeController implements Initializable {
 
     @FXML
     public void onActionBtnSave(ActionEvent actionEvent){
-        setInfoMessage(TF_dishname.getText());
 
+        // check if all text field have values
+        if (TF_dishname.getText().isEmpty() || TF_description.getText().isEmpty() || TF_dishtype.getText().isEmpty() ||
+            TF_preptime.getText().isEmpty() || TF_cooktime.getText().isEmpty()){
+            setErrorMessage("Please enter valid Values in the proper fields!");
+        }
+        else{
+            Recipe new_recipe = new Recipe();
 
+            new_recipe.setName(TF_dishname.getText());
+            new_recipe.setDescription(TF_description.getText());
+
+            switch (TF_dishtype.getText().toUpperCase()){
+
+                case "FISH":
+                    new_recipe.setType(MealType.FISH);
+                    break;
+                case "CHICKEN":
+                    new_recipe.setType(MealType.CHICKEN);
+                    break;
+                case "PORK":
+                    new_recipe.setType(MealType.PORK);
+                    break;
+                case "VEGAN":
+                    new_recipe.setType(MealType.VEGAN);
+                    break;
+                case "BEEF":
+                    new_recipe.setType(MealType.BEEF);
+                    break;
+                case "VEGETARIAN":
+                    new_recipe.setType(MealType.VEGETARIAN);
+                    break;
+                default:
+                    setErrorMessage("Choose one of the following meal types:   Fish, Chicken, Pork, Vergan, Beef, " +
+                            "Vegetarian");
+            }
+
+            try {
+                new_recipe.setPrepTime(Duration.ofMinutes(Integer.parseInt(TF_preptime.getText())));
+                new_recipe.setCookTime(Duration.ofMinutes(Integer.parseInt(TF_cooktime.getText())));
+
+            }catch (Exception e){
+                setErrorMessage("Please enter a valid number for cooking/preparation time (in minutes)");
+                e.printStackTrace();
+            }
+
+            // check if every attribute has a value
+            if (!(new_recipe.getName().isEmpty() || new_recipe.getDescription().isEmpty() || (new_recipe.getType()) == null
+                || (new_recipe.getPrepTime() == null) || (new_recipe.getCookTime() == null))){
+
+                RecipeManager.getInstance().addRecipe(new_recipe);
+
+                Stage stage = (Stage) TF_dishtype.getScene().getWindow();
+                stage.close();
+            }
+        }
     }
+
+    public void onActionbtnCancel(ActionEvent actionEvent){
+        Stage stage = (Stage) btn_cancel.getScene().getWindow();
+        stage.close();
+    }
+
 }
 
