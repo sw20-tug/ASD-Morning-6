5cdd00dc1181436ca05a56852625d9e1969d6f18
Lukas Kramer <kramer@student.tugraz.at>
2020-04-01 16:00:26 +0200

    COOK_002 implemented photo functionality [KL,SL]

--- src/app/controllers/MainController.java
+++ src/app/controllers/MainController.java
@@ -15,7 +15,7 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -82,6 +82,11 @@ public class MainController implements Initializable {
 
         deleteItem.setOnAction((event) -> {
             Recipe r = (Recipe) lv.getSelectionModel().getSelectedItem();
+            for(String s : r.getPhotos())
+            {
+                File f = new File(s);
+                f.deleteOnExit();
+            }
             RecipeManager.getInstance().deleteRecipe(r);
             fillListViews();
         });
