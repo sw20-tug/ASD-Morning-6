bdbe07e897c021e95a6a33e4332304db53d80843
Lukas Kramer <kramer@student.tugraz.at>
2020-04-22 11:02:53 +0200

    COOK_008 implemented detail view [KL,CC]

--- src/app/controllers/MainController.java
+++ src/app/controllers/MainController.java
@@ -4,6 +4,7 @@ import app.models.Recipe;
 import app.models.RecipeManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,11 +12,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -33,12 +42,45 @@ public class MainController implements Initializable {
     @FXML
     private ListView listFavRecipes;
 
+    @FXML
+    private ImageView imageViewDetails;
+
+    @FXML
+    private ImageView imgFav;
+
+    @FXML
+    private Button btnNextImage;
+
+    @FXML
+    private Button btnPrevImage;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Label lblDescription;
+
+    @FXML
+    private Label lblMealType;
+
+    @FXML
+    private Label lblPrepTime;
+
+    @FXML
+    private Label lblCookingTime;
+
+    @FXML
+    private AnchorPane apDetailView;
+
+
 
 
     private ObservableList<Recipe> recipeAllObservableList;
     private ObservableList<Recipe> recipeFavObservableList;
-
+    private int currentImage = 0;
+    public static Recipe currentRecipe;
     private FXMLLoader mLLoader;
+    private Image defaultRecipeImage;
 
     public MainController() {
 
@@ -49,14 +91,30 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        initViewElements();
-        fillListViews();
-        setListViewContextMenu(listAllRecipes);
-        setListViewContextMenu(listFavRecipes);
+        try {
+            initViewElements();
+            fillListViews();
+            updateDetailView();
+            setListViewContextMenu(listAllRecipes);
+            setListViewContextMenu(listFavRecipes);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void initViewElements() {
+    private void initViewElements() throws IOException {
+        FileInputStream input = new FileInputStream("appdata/images/dummy.png");
+        defaultRecipeImage = new Image(input, imageViewDetails.getFitWidth(), imageViewDetails.getFitHeight(), false, true);
+
+        input = new FileInputStream("appdata/images/star.png");
+        Image imageFav = new Image(input, imageViewDetails.getFitWidth(), imageViewDetails.getFitHeight(), false, true);
+        imgFav.setImage(imageFav);
+
         setInfoMessage("Welcome to COOK! - " + RecipeManager.getInstance().getRecipes().size() + " Recipes loaded");
+
+        input.close();
     }
 
     private void fillListViews() {
@@ -67,6 +125,22 @@ public class MainController implements Initializable {
         recipeFavObservableList = FXCollections.<Recipe>observableArrayList(RecipeManager.getInstance().getFavRecipes());
         listFavRecipes.setItems(recipeFavObservableList);
         listFavRecipes.setCellFactory(recipeListView -> new RecipeListViewCell());
+    }
+
+    private void updateDetailView() {
+        if(RecipeManager.getInstance().getRecipes().size() > 0) {
+            if(listAllRecipes.getSelectionModel().getSelectedItem() != null)
+            {
+                currentRecipe = (Recipe) listAllRecipes.getSelectionModel().getSelectedItem();
+            }else {
+                currentRecipe = RecipeManager.getInstance().getRecipes().firstElement();
+            }
+            displayCurrentRecipe();
+        }
+        else
+        {
+            apDetailView.setVisible(false);
+        }
     }
 
     private void setListViewContextMenu(ListView lv){
@@ -89,6 +163,7 @@ public class MainController implements Initializable {
             }
             RecipeManager.getInstance().deleteRecipe(r);
             fillListViews();
+            updateDetailView();
         });
         contextMenu.getItems().addAll(editItem,deleteItem);
 
@@ -137,12 +212,93 @@ public class MainController implements Initializable {
                 updateView();
             });
 
-            stageAddView.show();
+            stageAddView.showAndWait();
+            displayCurrentRecipe();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public void onActionBtnPrevImage(ActionEvent actionEvent) {
+        if(currentRecipe.getPhotos().size() > 1) {
+            if(currentImage <= 0)
+            {
+                currentImage = currentRecipe.getPhotos().size()-1;
+            }
+            else
+            {
+                currentImage--;
+            }
+            loadImage();
+        }
+    }
+
+    public void onActionBtnNextImage(ActionEvent actionEvent) {
+
+        if(currentRecipe.getPhotos().size() > 1) {
+            if(currentImage >= currentRecipe.getPhotos().size()-1)
+            {
+                currentImage = 0;
+            }
+            else
+            {
+                currentImage++;
+            }
+            loadImage();
+        }
+    }
+
+    private void loadImage() {
+        if (currentRecipe.getPhotos().size() > 0) {
+            try {
+                System.out.println("loading Image: " + currentRecipe.getPhotos().elementAt(currentImage));
+                FileInputStream input = new FileInputStream(currentRecipe.getPhotos().elementAt(currentImage));
+                Image img = new Image(input, imageViewDetails.getFitWidth(), imageViewDetails.getFitHeight(), false, true);
+                imageViewDetails.setImage(img);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            imageViewDetails.setImage(defaultRecipeImage);
+        }
+    }
+
+    private void displayCurrentRecipe(){
+        apDetailView.setVisible(true);
+        lblName.setText(currentRecipe.getName());
+        lblMealType.setText(currentRecipe.getType().toString());
+        lblPrepTime.setText(currentRecipe.getPrepTime().toMinutes() + " min.");
+        lblCookingTime.setText(currentRecipe.getCookTime().toMinutes() + " min.");
+        lblDescription.setText(currentRecipe.getDescription());
+        currentImage = 0;
+
+        if(currentRecipe.isFavourite()){
+            imgFav.setVisible(true);
+        }else{
+            imgFav.setVisible(false);
+        }
+
+        loadImage();
+        //TODO display instructions??
+
+    }
+
+    public void lvItemClicked(MouseEvent mouseEvent) {
+        currentRecipe = (Recipe) listAllRecipes.getSelectionModel().getSelectedItem();
+        if(currentRecipe != null){
+            displayCurrentRecipe();
+        }
+    }
+
+    public void lvItemFavClicked(MouseEvent mouseEvent) {
+        currentRecipe = (Recipe) listFavRecipes.getSelectionModel().getSelectedItem();
+        if(currentRecipe != null){
+            displayCurrentRecipe();
+        }
     }
 
     private void openEditView(Recipe r) {
@@ -165,7 +321,8 @@ public class MainController implements Initializable {
                 updateView();
             });
 
-            stageEditView.show();
+            stageEditView.showAndWait();
+            displayCurrentRecipe();
 
         } catch (IOException e) {
             e.printStackTrace();
