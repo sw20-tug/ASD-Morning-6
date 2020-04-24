1c0f32555d34cd21e91f577022e1a0ffb978e42f
LookaVooka <lord1601@gmail.com>
2020-04-24 17:07:52 +0200

    COOK-007 global search implemented [LW, LD]

--- src/app/controllers/MainController.java
+++ src/app/controllers/MainController.java
@@ -1,9 +1,12 @@
 package app.controllers;
 
+import app.models.Instruction;
 import app.models.Recipe;
 import app.models.RecipeManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,11 +17,14 @@ import javafx.scene.control.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import org.w3c.dom.Text;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Iterator;
 import java.util.ResourceBundle;
+import java.util.Vector;
 
 public class MainController implements Initializable {
     @FXML
@@ -32,6 +38,12 @@ public class MainController implements Initializable {
 
     @FXML
     private ListView listFavRecipes;
+
+    @FXML
+    private TextField searchAll;
+
+    @FXML
+    private TextField searchFav;
 
 
 
@@ -53,20 +65,107 @@ public class MainController implements Initializable {
         fillListViews();
         setListViewContextMenu(listAllRecipes);
         setListViewContextMenu(listFavRecipes);
+        searchListener();
     }
 
     private void initViewElements() {
         setInfoMessage("Welcome to COOK! - " + RecipeManager.getInstance().getRecipes().size() + " Recipes loaded");
     }
 
+    private void searchListener() {
+
+        FilteredList<Recipe> allFiltered;
+
+        allFiltered = new FilteredList<Recipe>(FXCollections.observableArrayList(RecipeManager.getInstance().getRecipes()));
+
+        searchAll.textProperty().addListener((observable, oldValue, newValue) -> {
+            allFiltered.setPredicate(recipe -> {
+
+                if (newValue.isEmpty())
+                    return true;
+
+                // search every attribute
+                if (recipe.getName().toLowerCase().contains(newValue.toLowerCase()))
+                    return true;
+                else if (recipe.getCookTime().toString().contains(newValue.toLowerCase()))
+                    return true;
+                else if (recipe.getPrepTime().toString().contains(newValue.toLowerCase()))
+                    return true;
+                else if (recipe.getDescription().toLowerCase().contains(newValue.toLowerCase()))
+                    return true;
+                else if (recipe.getType().toString().toLowerCase().contains(newValue.toLowerCase()))
+                    return true;
+
+                /* // search for instruction
+                Vector<Instruction> instructions = recipe.getCookInstructions();
+
+                // check every instruction for search term
+                for (int i = 0; i < recipe.getCookInstructions().size(); i++)
+                {
+                    if (instructions.get(i).getDescription().contains(newValue.toLowerCase()))
+                        return true;
+                    else if (instructions.get(i).getTask().contains(newValue.toLowerCase()))
+                        return true;
+                }
+                */
+
+                return false;
+            });
+        });
+
+        listAllRecipes.setItems(new SortedList<Recipe>(allFiltered));
+
+        FilteredList<Recipe> favFiltered;
+
+        favFiltered = new FilteredList<Recipe>(FXCollections.observableArrayList(RecipeManager.getInstance().getFavRecipes()));
+
+        searchFav.textProperty().addListener((observable, oldValue, newValue) -> {
+            favFiltered.setPredicate(recipe -> {
+
+                // search every attribute
+                if (recipe.getName().toLowerCase().contains(newValue.toLowerCase()))
+                    return true;
+                else if (recipe.getCookTime().toString().toLowerCase().contains(newValue.toLowerCase()))
+                    return true;
+                else if (recipe.getPrepTime().toString().toLowerCase().contains(newValue.toLowerCase()))
+                    return true;
+                else if (recipe.getDescription().toLowerCase().contains(newValue.toLowerCase()))
+                    return true;
+                else if (recipe.getType().toString().toLowerCase().contains(newValue.toLowerCase()))
+                    return true;
+
+                /* // search for instruction
+                Vector<Instruction> instructions = recipe.getCookInstructions();
+
+                // check every instruction for search term
+                for (int i = 0; i < recipe.getCookInstructions().size(); i++)
+                {
+                    if (instructions.get(i).getDescription().contains(newValue.toLowerCase()))
+                        return true;
+                    else if (instructions.get(i).getTask().contains(newValue.toLowerCase()))
+                        return true;
+                }
+                */
+                return false;
+            });
+        });
+
+        listFavRecipes.setItems(new SortedList<Recipe>(favFiltered));
+
+    }
+
+
+
     private void fillListViews() {
-        recipeAllObservableList = FXCollections.<Recipe>observableArrayList(RecipeManager.getInstance().getRecipes());
-        listAllRecipes.setItems(recipeAllObservableList);
+        //recipeAllObservableList = FXCollections.<Recipe>observableArrayList(RecipeManager.getInstance().getRecipes());
+        //listAllRecipes.setItems(recipeAllObservableList);
         listAllRecipes.setCellFactory(recipeListView -> new RecipeListViewCell());
 
-        recipeFavObservableList = FXCollections.<Recipe>observableArrayList(RecipeManager.getInstance().getFavRecipes());
-        listFavRecipes.setItems(recipeFavObservableList);
+        //recipeFavObservableList = FXCollections.<Recipe>observableArrayList(RecipeManager.getInstance().getFavRecipes());
+        //listFavRecipes.setItems(recipeFavObservableList);
         listFavRecipes.setCellFactory(recipeListView -> new RecipeListViewCell());
+
+        searchListener();
     }
 
     private void setListViewContextMenu(ListView lv){
