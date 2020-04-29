f870f5b23b499172c11af6273b706c7e520f4e04
Martin Laubreiter <laubreiter@student.tugraz.at>
2020-04-29 10:21:58 +0200

    COOK_012 (#26) Added Tests for MainController filtering (CF,ML)

--- src/app/controllers/MainController.java
+++ src/app/controllers/MainController.java
@@ -105,9 +105,6 @@ public class MainController implements Initializable {
     @FXML
     private AnchorPane apDetailView;
 
-
-
-
     private ObservableList<Recipe> recipeAllObservableList;
     private ObservableList<Recipe> recipeFavObservableList;
     private int currentImage = 0;
@@ -117,6 +114,16 @@ public class MainController implements Initializable {
 
     public MainController() {
 
+    }
+
+    public MainController(Label lblMessage, ComboBox<MealType> cmbMealType, Slider sliderPrepTime, Slider sliderCookTime, ListView listAllRecipes, ListView listFavRecipes, Button btnUndoFilter) {
+        this.lblMessage = lblMessage;
+        this.cmbMealType = cmbMealType;
+        this.sliderPrepTime = sliderPrepTime;
+        this.sliderCookTime = sliderCookTime;
+        this.listAllRecipes = listAllRecipes;
+        this.listFavRecipes = listFavRecipes;
+        this.btnUndoFilter = btnUndoFilter;
     }
 
     @FXML
@@ -444,7 +451,7 @@ public class MainController implements Initializable {
 
     }
 
-    private void filterRecipes(){
+    public void filterRecipes(){
         btnUndoFilter.setDisable(false);
         MealType chosenMealType = (MealType) cmbMealType.getSelectionModel().getSelectedItem();
         int prepTimeMin = (int) Math.round(sliderPrepTime.getValue());
---
+++ src/test/MainControllerTest.java
@@ -0,0 +1,133 @@
+package test;
+
+import app.controllers.MainController;
+import app.enums.MealType;
+import app.models.Recipe;
+import app.models.RecipeManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+
+import java.time.Duration;
+
+import static org.mockito.Mockito.spy;
+
+@ExtendWith(ApplicationExtension.class)
+public class MainControllerTest {
+
+    private ComboBox<MealType> cmbMealType;
+    //max_time
+    private Slider sliderPrepTime;
+    //max_time
+    private Slider sliderCookTime;
+    private ListView listAllRecipes;
+    private ListView listFavRecipes;
+    private Button btnUndoFilter;
+    private Label lblMessage;
+
+    private MainController controller;
+
+    static RecipeManager recipeManager;
+
+    @BeforeEach
+    public void setup() {
+        ObservableList<MealType> mealTypes = FXCollections.observableArrayList(MealType.FISH, MealType.PORK, MealType.VEGAN, MealType.BEEF, MealType.CHICKEN, MealType.VEGETARIAN);
+
+        cmbMealType = spy(new ComboBox<>());
+        cmbMealType.setItems(mealTypes);
+
+        sliderPrepTime = spy(new Slider(0, 200, 0));
+        sliderCookTime = spy(new Slider(0, 200, 0));
+        listAllRecipes = spy(new ListView());
+        listFavRecipes = spy(new ListView());
+        btnUndoFilter = spy(new Button());
+        lblMessage = spy(new Label());
+
+
+        Recipe recipe1 = new Recipe("test1", "test1 desc", MealType.FISH, Duration.ofMinutes(5), Duration.ofMinutes(30));
+        Recipe recipe2 = new Recipe("test2", "test2 desc", MealType.PORK, Duration.ofMinutes(7), Duration.ofMinutes(5));
+        Recipe recipe3 = new Recipe("test3", "test3 desc", MealType.BEEF, Duration.ofMinutes(15), Duration.ofMinutes(15));
+        Recipe recipe4 = new Recipe("test4", "test4 desc", MealType.VEGAN, Duration.ofMinutes(4), Duration.ofMinutes(14));
+        Recipe recipe5 = new Recipe("test5", "test5 desc", MealType.VEGETARIAN, Duration.ofMinutes(30), Duration.ofMinutes(6));
+        Recipe recipe6 = new Recipe("test6", "test6 desc", MealType.FISH, Duration.ofMinutes(20), Duration.ofMinutes(10));
+        Recipe recipe7 = new Recipe("test7", "test7 desc", MealType.PORK, Duration.ofMinutes(7), Duration.ofMinutes(18));
+        Recipe recipe8 = new Recipe("test8", "test8 desc", MealType.FISH, Duration.ofMinutes(6), Duration.ofMinutes(20));
+        Recipe recipe9 = new Recipe("test9", "test9 desc", MealType.FISH, Duration.ofMinutes(70), Duration.ofMinutes(5));
+        Recipe recipe10 = new Recipe("test10", "test10 desc", MealType.FISH, Duration.ofMinutes(5), Duration.ofMinutes(70));
+        Recipe recipe11 = new Recipe("test11", "test11 desc", MealType.FISH, Duration.ofMinutes(20), Duration.ofMinutes(11));
+
+        recipeManager = new RecipeManager();
+
+        recipeManager.addRecipe(recipe1);
+        recipeManager.addRecipe(recipe2);
+        recipeManager.addRecipe(recipe3);
+        recipeManager.addRecipe(recipe4);
+        recipeManager.addRecipe(recipe5);
+        recipeManager.addRecipe(recipe6);
+        recipeManager.addRecipe(recipe7);
+        recipeManager.addRecipe(recipe8);
+        recipeManager.addRecipe(recipe9);
+        recipeManager.addRecipe(recipe10);
+        recipeManager.addRecipe(recipe11);
+
+        RecipeManager.setInstance(recipeManager);
+
+        controller = new MainController(lblMessage, cmbMealType, sliderPrepTime, sliderCookTime, listAllRecipes,listFavRecipes, btnUndoFilter);
+    }
+
+    @Test
+    void testFilterNoneFinding() {
+
+        cmbMealType.getSelectionModel().select(MealType.FISH);
+        sliderPrepTime.setValue(100);
+        sliderCookTime.setValue(100);
+
+        controller.filterRecipes();
+
+        assert listAllRecipes.getItems().size() == 0;
+
+    }
+
+    @Test
+    void testFilterAllFishFinding() {
+
+        cmbMealType.getSelectionModel().select(MealType.FISH);
+        sliderPrepTime.setValue(0);
+        sliderCookTime.setValue(0);
+
+        controller.filterRecipes();
+
+        assert listAllRecipes.getItems().size() == 6;
+
+    }
+
+    @Test
+    void testFilterFindBasic() {
+
+        cmbMealType.getSelectionModel().select(MealType.FISH);
+        sliderPrepTime.setValue(20);
+        sliderCookTime.setValue(5);
+
+        controller.filterRecipes();
+
+        assert listAllRecipes.getItems().size() == 3;
+
+    }
+
+    @Test
+    void testFilterFindBasic2() {
+
+        cmbMealType.getSelectionModel().select(MealType.FISH);
+        sliderPrepTime.setValue(20);
+        sliderCookTime.setValue(11);
+
+        controller.filterRecipes();
+
+        assert listAllRecipes.getItems().size() == 1;
+
+    }
+}
